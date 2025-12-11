0faf9ef3008b554aeaffddbf24b5f13a36e5dfad
Alexander <developer.alexander@outlook.com>
2025-12-11 07:14:48 -0500

    feat: add skills section with icons and components

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,10 +6,11 @@ import { HeroComponent } from '@features/home/components/hero/hero.component';
 import { AboutComponent } from '@features/about/components/about/about.component';
 import { ExperienceComponent } from '@features/experience/components/experience/experience.component';
 import { ProjectsComponent } from '@features/project/components/projects/projects.component';
+import { SkillsComponent } from '@features/skills/components/skills/skills.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LayoutComponent, HeroComponent, AboutComponent, ExperienceComponent, ProjectsComponent],
+  imports: [RouterOutlet, LayoutComponent, HeroComponent, AboutComponent, ExperienceComponent, ProjectsComponent, SkillsComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/app/features/skills/components/skills/skills.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { SvgIconComponent } from 'angular-svg-icon';
+
+@Component({
+  selector: 'app-skills',
+  imports: [
+    SvgIconComponent
+  ],
+  templateUrl: './skills.component.html',
+  styleUrl: './skills.component.scss',
+})
+export class SkillsComponent {
+
+}
